f3e8b2e12645e7a29fcb0bae8086e2a8cba6eb2f
Kelsey Guffanti <kc.breen3@gmail.com>
2023-07-25 15:07:38 -0400

    config for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,52 +1,12 @@
-// "use strict";
-// /** Database setup for whisk. */
-// // const { Client } = require("pg");
-// // import { getDatabaseUri } from "./config.js";
-
-// import pkg from "pg";
-// const { Client } = pkg;
-
-// const db = new Client(process.env.DATABASE_URL);
-
-// db.connect(function (err) {
-//   if (err) {
-//     return console.error("could not connect to postgres", err);
-//   }
-//   db.query('SELECT NOW() AS "theTime"', function (err, result) {
-//     if (err) {
-//       return console.error("error running query", err);
-//     }
-//     console.log(result.rows[0].theTime);
-//     // >> output: 2018-08-23T14:02:57.117Z
-//   });
-// });
-
-// // const Pool = require("pg").Pool;
-// // const db = new Pool({
-// //   user: "kelseyguffanti",
-// //   host: "localhost",
-// //   database: getDatabaseUri(),
-// //   //   password: "password",
-// //   port: 5432,
-// // });
-
-// export default db;
-
-// FOR RUNNING TESTS:
 "use strict";
 /** Database setup for whisk. */
 // const { Client } = require("pg");
-import { getDatabaseUri } from "./config.js";
+// import { getDatabaseUri } from "./config.js";
 
 import pkg from "pg";
 const { Client } = pkg;
 
-const db = new Client({
-  host: "localhost",
-  port: 5432,
-  database: getDatabaseUri(),
-  user: "kelseyguffanti",
-});
+const db = new Client(process.env.DATABASE_URL);
 
 db.connect(function (err) {
   if (err) {
@@ -71,3 +31,32 @@ db.connect(function (err) {
 // });
 
 export default db;
+
+// // FOR RUNNING TESTS:
+// "use strict";
+// /** Database setup for whisk. */
+// // const { Client } = require("pg");
+// import { getDatabaseUri } from "./config.js";
+
+// import pkg from "pg";
+// const { Client } = pkg;
+
+// const db = new Client({
+//   host: "localhost",
+//   port: 5432,
+//   database: getDatabaseUri(),
+//   user: "kelseyguffanti",
+// });
+
+// db.connect();
+
+// // const Pool = require("pg").Pool;
+// // const db = new Pool({
+// //   user: "kelseyguffanti",
+// //   host: "localhost",
+// //   database: getDatabaseUri(),
+// //   //   password: "password",
+// //   port: 5432,
+// // });
+
+// export default db;
